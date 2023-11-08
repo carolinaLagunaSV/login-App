25472e05b96f6ec93d6f3a48b3d4949b050f2b2f
Carolina Laguna <114037038+carolinaLagunaSV@users.noreply.github.com>
2023-11-07 23:42:11 -0500

    10. FireBase

---
+++ 07-bonus-loginApp/src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyAdh-F9noi1iVVYLZSwGGfRji-S7jkIa4w ';
+
+
+  // CREAR NUEVOS USUARIOS
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+
+  constructor( private http:HttpClient ) { }
+
+  logout() {
+
+  }
+
+  login( usuario: UsuarioModel ) {
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel ) {
+
+  }
+}
